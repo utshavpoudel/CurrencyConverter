faf28a8f1b08d9a52a1afc4553ddffe3dd0cd0e5
utshav88 <poudelutshav333@gmail.com>
2026-06-21 21:05:02 +0545

    addition in features

--- comb.js
+++ comb.js
@@ -20,3 +20,41 @@ swapBtn.addEventListener("click", () => {
 });
 
 
+const amountInput = document.querySelector("#amount");
+const result = document.querySelector("#result");
+
+function convertCurrency() {
+
+    let amount = parseFloat(amountInput.value);
+
+    if (isNaN(amount)) {
+        result.innerText = "0";
+        return;
+    }
+
+    let convertedAmount;
+
+    // usd to npr
+    if (fromCurrency.value === "usd" && toCurrency.value === "nep") {
+        convertedAmount = amount * 150;
+        result.innerText = `${convertedAmount.toFixed(2)} NPR`;
+    }
+
+    // npr to usD
+    else if (fromCurrency.value === "nep" && toCurrency.value === "usd") {
+        convertedAmount = amount / 150;
+        result.innerText = `${convertedAmount.toFixed(2)} USD`;
+    }
+
+    // Same to same eg: usd to usd
+    else {
+        result.innerText = `${amount.toFixed(2)}`;
+    }
+}
+
+
+amountInput.addEventListener("input", convertCurrency);
+
+fromCurrency.addEventListener("change", convertCurrency);
+
+toCurrency.addEventListener("change", convertCurrency);
